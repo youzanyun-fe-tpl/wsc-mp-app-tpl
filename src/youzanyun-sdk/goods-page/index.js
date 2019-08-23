6359104ff2d305e3059e07f4c87b0cccb755f26a
songweite <songweite@youzan.com>
2019-08-23 17:04:51 +0800

    处理

--- src/youzanyun-sdk/goods-page/index.js
+++ src/youzanyun-sdk/goods-page/index.js
@@ -1,117 +1,24 @@
+import openData from './data-mock.js';
+
+function Event() {
+  this.on = () => {
+    console.log('events callback');
+  };
+
+  this.trigger = () => {
+    console.log('trigger events');
+  };
+}
+
 export default {
-  openData: {
-    goodsInfo: {
-      video: { width: null, height: null, coverUrl: '', videoUrl: '', countPlayedUrl: '' },
-      formattedPictures: [
-        'https://img.yzcdn.cn/upload_files/2018/12/06/4a111da2cb6aaee1bfb55248c396f136.jpg?imageView2/2/w/730/h/0/q/75/format/webp'
-      ],
-      skuData: {
-        collectionId: 36260037,
-        collectionPrice: 0,
-        hideStock: false,
-        list: [
-          {
-            code: '12',
-            combineSku: false,
-            discount: 100,
-            discountPrice: 0,
-            id: 36332972,
-            itemId: 446351820,
-            kdtId: 16719442,
-            oldPrice: 100,
-            orderNum: 0,
-            price: 80,
-            priceTitle: '会员折扣',
-            s1: 3375,
-            s2: 0,
-            s3: 0,
-            s4: 0,
-            s5: 0,
-            sendNum: 0,
-            sku: '[{"k":"蓝色","k_id":3,"v":"14","v_id":3375}]',
-            soldNum: 0,
-            stockNum: 444
-          },
-          {
-            code: '34',
-            combineSku: false,
-            discount: 100,
-            discountPrice: 0,
-            id: 36332971,
-            itemId: 446351820,
-            kdtId: 16719442,
-            oldPrice: 200,
-            orderNum: 0,
-            price: 160,
-            priceTitle: '会员折扣',
-            s1: 334593,
-            s2: 0,
-            s3: 0,
-            s4: 0,
-            s5: 0,
-            sendNum: 0,
-            sku: '[{"k":"蓝色","k_id":3,"v":"199","v_id":334593}]',
-            soldNum: 0,
-            stockNum: 555
-          }
-        ],
-        maxPrice: 160,
-        messages: [],
-        minPrice: 80,
-        noneSku: false,
-        oldPrice: '1.00 - 2.00',
-        price: '0.80 - 1.60',
-        soldNum: 12,
-        stockNum: 999,
-        tree: [
-          {
-            count: 2,
-            k: '蓝色',
-            kId: 3,
-            kS: 's1',
-            v: [{ id: 3375, name: '14' }, { id: 334593, name: '199' }]
-          }
-        ]
-      },
-      goods: {
-        id: 446351820,
-        type: 'normal',
-        alias: '3f57gh5jcafia',
-        title: '秒杀wq',
-        path:
-          'packages/goods/detail/index?alias=3f57gh5jcafia&banner_id=f.78127763~goods.2~4~bxGkcNqJ&components_style_size=0',
-        picture: 'https://img.yzcdn.cn/upload_files/2018/12/06/4a111da2cb6aaee1bfb55248c396f136.jpg',
-        sellPoint: '',
-        origin: '',
-        isVirtual: false,
-        isVirtualTicket: false,
-        isInstallment: false,
-        isDisplay: 1,
-        limitBuy: false,
-        waitToSoldTime: 0,
-        buyWay: 1,
-        buyUrl: '',
-        forbidBuyReason: '',
-        isSupportFCode: false,
-        risk: { match: false, note: '' }
-      },
-      goodsPrice: {
-        isRange: true,
-        currentPrice: '0.80 - 1.60',
-        minPriceData: { yuan: '0', fen: '80', desc: '0.80' },
-        maxPriceData: { yuan: '1', fen: '60', desc: '1.60' },
-        originPrice: '1.00 - 2.00'
-      },
-      goodsSubTitle: '',
-      goodsTag: '',
-      shopName: '心怡画坊',
-      shopLogo:
-        'https://img.yzcdn.cn/upload_files/2019/05/09/Fo6s6q_dDNOr5TduBBpEsDjx-j5n.jpeg?imageView2/2/w/100/h/100/q/75/format/webp',
-      shopCertTags: [],
-      showcaseComponents: [{ type: 'rich_text', content: '', key: '0.3560-rich_text' }],
-      cartInfo: { count: 1 },
-      imPath:
-        'packages/goods/detail/index?alias=3f57gh5jcafia&banner_id=f.78127763~goods.2~4~bxGkcNqJ&components_style_size=0&imFrom=GOODS&price=0.80 - 1.60&originPrice=1.00 - 2.00'
-    }
+  openData,
+  process: {
+    showSKU(type) {
+      return Promise.resolve();
+    },
+  },
+  events: {
+    beforeCartSubmit: new Event(),
+    afterBuy: new Event(),
   }
 };
