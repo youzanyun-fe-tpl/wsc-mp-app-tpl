906971f943463969d55b1514b571a690b4494926
taiyong <taiyong@youzan.com>
2019-08-28 15:08:15 +0800

    feature

--- src/pages/home/feature/index.js
+++ src/pages/home/feature/index.js
@@ -1,6 +1,7 @@
 Page({
   data: {
-    components: [
+    components: [ // 数组中的item 会作为component-data属性传入组件中， 字段值与pc端保持一致。
+      // 修改这部分数据可以进行自定义组件测试开发
       {
         "type": "custom_test",
         "text": "1元抽奖"
