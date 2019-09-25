ed33097286d02609365165fe345efb35c0c73244
xuzhuoya <xuzhuoya@youzan.com>
2019-09-25 15:28:45 +0800

    fix: update ecloud-space

--- src/ecloud-space/index.js
+++ src/ecloud-space/index.js
@@ -1,18 +1,2 @@
 export default function (sdk) {
-  const user = sdk.app.user;
-
-  sdk.app.on('ecloud:login:success', userInfo => {
-    judgeUserNavigate(userInfo);
-  });
-
-  function judgeUserNavigate(userInfo) {
-    console.log('-------global judgeUserNavigate', userInfo.mobile);
-    // 如果是已注册
-    if (userInfo.mobile) {
-      let redirectUrl = '';
-      sdk.navigate('login', { redirectUrl }, 'redirectTo');
-    } else {
-      // TODO nothing 正常加载
-    }
-  }
 }
