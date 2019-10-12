5c9f379aae9c78fed618492deaa1811f51262bb6
taiyong <taiyong@youzan.com>
2019-10-12 16:59:50 +0800

    init

---
+++ src/pages/usercenter/dashboard/custom-components/test/index.js
@@ -0,0 +1,20 @@
+Component({
+  behaviors: [],
+  properties: {
+  },
+  data: {},
+  lifetimes: {
+    attached: function attached() {},
+    moved: function moved() {},
+    detached: function detached() {}
+  },
+  ready: function ready() {
+   
+  },
+  pageLifetimes: {
+    show: function show() {}
+  },
+  methods: {
+    
+  }
+});
